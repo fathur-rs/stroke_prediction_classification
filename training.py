63d18838249fe63824852c812e764d53fe14cf31
Fathurrahman Syarief <fathurrahmansyarief.gmail.com>
2021-06-22 13:41:25 +0700

    changing model criterion to 'gini' and fixing some index error :(

--- training.py
+++ training.py
@@ -7,19 +7,18 @@ from sklearn.metrics import classification_report
 from terminaltables import SingleTable
 import os
 
-def splitting_data(data):
-    X = data.drop(['id', 'stroke'], axis=1)
-    Y = data.stroke
+def splitting_data(df):
+    X = df.drop(['id', 'stroke'], axis=1)
+    Y = df.stroke
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.29, random_state=42)
     print('Split the data...'), time.sleep(1)
     return resampling(X_train, X_test, y_train, y_test)
 
 def resampling(X_train, X_test, y_train, y_test):
     print('Resampling Train Data...'), time.sleep(1)
+    smote = SMOTE(sampling_strategy='minority', random_state=0)
 
-    smote = SMOTE(sampling_strategy='minority', random_state=42)
     X_train_resample, y_train_resample = smote.fit_resample(X_train, y_train)
-
     table = SingleTable([
         ['Stroke', 'Sebelum Resampling', 'Sesudah Resampling'],
         [0, sum(y_train == 0), sum(y_train_resample == 0)],
@@ -37,7 +36,6 @@ def model(X_train_resample,X_test, y_train_resample, y_test):
 
     Y_pred = clf.predict(X_test)
     print(f'Metrics:\n{classification_report(y_test, Y_pred)}'), time.sleep(2)
-
     return saving_model(clf)
 
 def saving_model(clf):
