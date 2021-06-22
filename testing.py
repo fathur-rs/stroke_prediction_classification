88136c6799fd2c7b729670cab51f4dd46c6dae8c
Fathurrahman Syarief <fathurrahmansyarief.gmail.com>
2021-06-22 13:24:39 +0700

    change cat.codes to label encoder

--- testing.py
+++ testing.py
@@ -2,6 +2,7 @@ import pandas as pd
 pd.options.mode.chained_assignment = None
 import training
 import time
+from sklearn import preprocessing
 
 def data():
     url = 'https://raw.githubusercontent.com/fathur-rs/uas/master/healthcare-dataset-stroke-data%20(2).csv'
@@ -12,20 +13,14 @@ def data():
 def data_clean(data):
     df = data.loc[data["gender"] != 'Other']
     df.dropna(axis=0, inplace=True)
-    obj_df = df.select_dtypes(include=['object']).copy()
-    return data_prep(obj_df, df)
+    return data_prep(df)
 
-def data_prep(obj_df, df):
-    df = df
-    for col in ['gender', 'ever_married', 'work_type', 'Residence_type', 'smoking_status']:
-        obj_df[col] = obj_df[col].astype('category')
-        obj_df[col] = obj_df[col].cat.codes
+def data_prep(df):
+    label_encoder = preprocessing.LabelEncoder()
+    for i in ['gender', 'ever_married', 'work_type', 'Residence_type', 'smoking_status']:
+        df[i] = label_encoder.fit_transform(df[i])
     print('Data Preprocessing...'), time.sleep(1)
-    return data_to_model(obj_df, df)
-
-def data_to_model(obj_df, df):
-    df_final = df.iloc[:, [0, 2, 3, 4, 8, 9, 11]].join(obj_df)
-    return training.splitting_data(df_final)
+    return training.splitting_data(df)
 
 if __name__ == '__main__':
     data()
