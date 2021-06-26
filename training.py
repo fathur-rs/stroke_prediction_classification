b0072db9e39d632129c30643deef0f8f61447896
Fathurrahman Syarief <fathurrahmansyarief.gmail.com>
2021-06-26 13:02:27 +0700

    adding new variabel for building tabel

--- training.py
+++ training.py
@@ -17,13 +17,11 @@ def data():
     return data_clean(data)
 
 def data_clean(data):
-    # exclude gender 'Other' dan drop NaN Values
     df = data.loc[data["gender"] != 'Other']
     df.dropna(axis=0, inplace=True)
     return data_prep(df)
 
 def data_prep(df):
-    # mengubah data object/category menjadi integer
     label_encoder = preprocessing.LabelEncoder()
     obj_df = ['gender', 'ever_married', 'work_type', 'Residence_type', 'smoking_status']
     for i in obj_df:
@@ -34,44 +32,32 @@ def data_prep(df):
 def splitting_data(df):
     X = df.drop(['id', 'stroke'], axis=1)
     Y = df.stroke
-
-    # splitting atribut dan atribut target menjadi train dan test
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.29, random_state=42)
     print('Split the data...'), time.sleep(1)
     return resampling(X_train, X_test, y_train, y_test)
 
 def resampling(X_train, X_test, y_train, y_test):
     print('Resampling Train Data...'), time.sleep(1)
-
-    # resampling x_train dan y_train
     smote = SMOTE(sampling_strategy='minority', random_state=0)
     X_train_resample, y_train_resample = smote.fit_resample(X_train, y_train)
-
-    # print output sebelum resampling dan sesudah resampling dalam bentuk tabel
-    table = SingleTable([
-        ['Stroke', 'Sebelum Resampling', 'Sesudah Resampling'],
-        [0, sum(y_train == 0), sum(y_train_resample == 0)],
-        [1, sum(y_train == 1), sum(y_train_resample == 1)]
-    ], title='Y_Train')
+    col_name = ['Stroke', 'Sebelum Resampling', 'Sesudah Resampling']
+    row_one = [0, sum(y_train == 0), sum(y_train_resample == 0)]
+    row_two = [1, sum(y_train == 1), sum(y_train_resample == 1)]
+    header = 'Stroke Frequencies'
+    table = SingleTable([col_name,row_one,row_two], title=header)
     print(table.table), time.sleep(2)
-
     return model(X_train_resample,X_test, y_train_resample, y_test)
 
 def model(X_train_resample,X_test, y_train_resample, y_test):
     print('Fit Data to Model...'), time.sleep(1)
     print('Train the Data...'), time.sleep(1)
-
-    # inisiasi model decision tree dan masukkan x_train_resample dan y_train resample kedalam model
     clf = DecisionTreeClassifier(criterion='gini', random_state=42).fit(X_train_resample, y_train_resample)
     print('Data Trained...'), time.sleep(1)
-
-    # machine learning evaluation metric, Accuracy, F1-Score, Recall, Precision
     Y_pred = clf.predict(X_test)
     print(f'Metrics:\n{classification_report(y_test, Y_pred)}'), time.sleep(2)
     return saving_model(clf)
 
 def saving_model(clf):
-    # saving model decision tree dengan library pickle
     with open('module_decision', 'wb') as f:
         pickle.dump(clf, f)
         print('Saving Model...'), time.sleep(1)
@@ -80,6 +66,5 @@ def saving_model(clf):
 
 
 if __name__ == '__main__':
-    # call function data()
     data()
 
